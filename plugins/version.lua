4558f007c786534ce7c1617274ff8b51aeb99fad
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-10 02:05:38 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,13 +2,12 @@ do
 
 function run(msg, matches)
   return [[ *SpheroBoT Helper*
-  
 Version: 2.2
 Developer 
 And
 Manager : @MrBlackLife]]
 end
-
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 return {
   description = "Shows bot version", 
   usage = "!version: Shows bot version",
