30d14c944fca7e4a59bbcbeb57291870a991ca34
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-10 14:40:33 +0430

    Update link.lua

--- plugins/link.lua
+++ plugins/link.lua
@@ -1,9 +1,10 @@
 do
 
 local function run(msg ,matches)
-if matches[1]:lower() == 'gplink' and is_momod(msg) then
+ matches[1]:lower() == 'gplink' and is_momod(msg) then
 return '[Link For "..msg.to.title.."]("..group_link..")'
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
 end
 end
 return {
